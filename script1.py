bd7f68d61de1b09664b040eeba5bcb63a5b31679
Mason Edwards <Mason_Edawrds2@baylor.edu>
2023-08-29 15:18:42 -0500

    Resolve repos

--- script1.py
+++ script1.py
@@ -2,15 +2,15 @@
 # Correc the mistakes and produce the following output:
 # Johnny, the total of your purchase is $1,688.70
 
-def calcTotal(amt,tax,name):
-    mytotal = amt * (1+tax)
-    print(f"{myname}, the total of your purchase is ${total:,.2f}")
-
 myname = 'Johnny'
-price = 1,560
-thetax = 8.25%
+price = 1560
+tax = .0825
 
-calcTotal(thetax,price,name)
+def calcTotal(price,tax,myname):
+    mytotal = price * (1+tax)
+    print(f"{myname}, the total of your purchase is ${mytotal:,.2f}")
+
+calcTotal(price,tax,myname)
 
 
 
@@ -21,7 +21,9 @@ calcTotal(thetax,price,name)
 list1 = [1,'two',3.2,'four',5]
 
 for i in list1:
-    print(i)
+    if type(i) == int:
+        print(i)
+
 
 
 
@@ -54,3 +56,4 @@ my_layout = Layout(title='Baylor University',geo_scope='usa')
 fig = {'data':data, 'layout':my_layout}
 
 offline.plot(fig,filename='Baylor.html')
+
